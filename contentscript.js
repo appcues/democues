6bbffe4c6776370a161aba5625004c171978aacc
Matthew O'Gorman <mog@rldn.net>
2018-11-22 02:43:24 -0500

    starting to play with storage api seems easy enough

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,20 @@
-console.log("Appending Appcues Javascript...");
-Appcues.identify("mog2");
-console.log("done appWindow", window);
+console.log("Democues begin...");
+// chrome.storage.sync.clear(function() {
+//     // Notify that we saved.
+//     console.log('Settings cleared');
+// });
+chrome.storage.sync.get(['appcutie'], function(result) {
+    if(result.appcutie === undefined) {
+        const appcutie = {appcutie: { accountId: '42328', userId: 'mog'}};
+        chrome.storage.sync.set(appcutie, function() {
+            // Notify that we saved.
+            console.log('Settings saved');
+            console.log('is appcues loaded?', Appcues);
+            Appcues.identify(appcutie.userId);
+        });
+    } else {
+        console.log('Value currently is ' + JSON.stringify(result.appcutie));
+        console.log('is appcues loaded?', Appcues);
+        Appcues.identify(result.appcutie.userId);
+    }
+});
